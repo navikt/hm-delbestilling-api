e94217e4ccf9e9a4ee13549ddf5f3f51bcaebc91
Ole Steinar Lillestøl Skrede <Ole.Steinar.Lillestol.Skrede@nav.no>
2023-06-22 13:13:45 +0200

    fix db og bruk postrgescontainers i test

--- src/main/resources/db/migration/V1__CREATE_TABLES.sql
+++ src/main/resources/db/migration/V1__CREATE_TABLES.sql
@@ -4,6 +4,6 @@ CREATE TABLE IF NOT EXISTS delbestilling
     brukers_kommunenr  varchar(4)  NOT NULL,
     fnr_bruker         varchar(11) NOT NULL,
     fnr_bestiller      varchar(11) NOT NULL,
-    delbestilling_json text        NOT NULL,
-    opprettet_dato     TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    delbestilling_json jsonb       NOT NULL,
+    opprettet          TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
