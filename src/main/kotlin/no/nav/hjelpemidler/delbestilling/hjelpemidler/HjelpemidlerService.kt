c78aacc4427b67a0f07fc318da5e96abe9422a0d
Torbjørn Vallestad <torbval@gmail.com>
2023-06-26 13:57:30 +0200

    fikset etter review

--- src/main/kotlin/no/nav/hjelpemidler/delbestilling/hjelpemidler/HjelpemidlerService.kt
+++ src/main/kotlin/no/nav/hjelpemidler/delbestilling/hjelpemidler/HjelpemidlerService.kt
@@ -5,7 +5,6 @@ import no.nav.hjelpemidler.delbestilling.delbestilling.AlleHjelpemidlerMedDelerR
 
 class HjelpemidlerService {
 
-
     suspend fun hentAlleHjelpemidlerMedDeler(): AlleHjelpemidlerMedDelerResultat {
         val alleHjelpemidlerMedDeler = HjelpemiddelDeler.hentAlleHjelpemidlerMedDeler()
         return AlleHjelpemidlerMedDelerResultat(alleHjelpemidlerMedDeler, HttpStatusCode.OK)
