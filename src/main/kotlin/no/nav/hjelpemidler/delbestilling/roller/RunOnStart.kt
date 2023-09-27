8e727e09b6964e6da4d55698e13ca161e69a3731
Håkon <hakonhagen2@gmail.com>
2023-09-27 15:33:09 +0200

    Update RunOnStart.kt

--- src/main/kotlin/no/nav/hjelpemidler/delbestilling/roller/RunOnStart.kt
+++ src/main/kotlin/no/nav/hjelpemidler/delbestilling/roller/RunOnStart.kt
@@ -16,31 +16,34 @@ class RunOnStart(
 
             var antallOppdaterteRader = 0
             delbestillinger.forEach { lagretDelbestilling ->
-                // logg.info { "opprinnelig delbestilling: $lagretDelbestilling" }
 
                 val navnHovedprodukt =
                     HjelpemiddelDeler.hentHjelpemiddelMedDeler(lagretDelbestilling.delbestilling.hmsnr)?.navn
 
-                // logg.info { "navnHovedprodukt: $navnHovedprodukt" }
-
                 if (navnHovedprodukt == null) {
-                    logg.info { "Klarte ikke å finne navn for ${lagretDelbestilling.delbestilling.hmsnr}" }
+                    logg.info { "Klarte ikke å finne navn for ${lagretDelbestilling.delbestilling.hmsnr}, hoppper over..." }
+                    return@forEach
                 }
 
-                if (lagretDelbestilling.delbestilling.navn == null && navnHovedprodukt != null) {
-                    delbestillingRepository.withTransaction { tx ->
-                        val oppdatertDelbestilling = lagretDelbestilling.delbestilling.copy(navn = navnHovedprodukt)
-                        // TODO: for debugging, må fjernes før merges til prod
-                        val oppdaterSaksnummer = "46"
-                        if (lagretDelbestilling.saksnummer == oppdaterSaksnummer.toLong()) {
-                            delbestillingRepository.oppdaterDelbestillingUtenSistOppdatert(
-                                tx,
-                                lagretDelbestilling.saksnummer,
-                                oppdatertDelbestilling
-                            )
-                            logg.info("Oppdatert delbestilling for saksnummer $oppdaterSaksnummer: $oppdatertDelbestilling")
-                            antallOppdaterteRader++
-                        }
+                if (lagretDelbestilling.delbestilling.navn != null) {
+                    logg.info { "lagretBestilling med saksnummer ${lagretDelbestilling.saksnummer} har allerede navn=${lagretDelbestilling.delbestilling.navn}, hopper over..." }
+                    return@forEach
+                }
+
+                logg.info { "Oppdater navn til \"$navnHovedprodukt\" for ${lagretDelbestilling.delbestilling.hmsnr} " }
+
+                delbestillingRepository.withTransaction { tx ->
+                    val oppdatertDelbestilling = lagretDelbestilling.delbestilling.copy(navn = navnHovedprodukt)
+                    // TODO: for debugging, må fjernes før merges til prod
+                    val oppdaterSaksnummer = "46"
+                    if (lagretDelbestilling.saksnummer == oppdaterSaksnummer.toLong()) {
+                        delbestillingRepository.oppdaterDelbestillingUtenSistOppdatert(
+                            tx,
+                            lagretDelbestilling.saksnummer,
+                            oppdatertDelbestilling
+                        )
+                        logg.info("Oppdatert delbestilling for saksnummer $oppdaterSaksnummer: $oppdatertDelbestilling")
+                        antallOppdaterteRader++
                     }
                 }
             }
