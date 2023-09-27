c056db829d3505e388b51e1fa96ee002792e4359
Håkon <hakonhagen2@gmail.com>
2023-09-27 13:37:49 +0200

    funker dette?

--- src/main/kotlin/no/nav/hjelpemidler/delbestilling/roller/RunOnStart.kt
+++ src/main/kotlin/no/nav/hjelpemidler/delbestilling/roller/RunOnStart.kt
@@ -5,13 +5,22 @@ import kotliquery.sessionOf
 import kotliquery.using
 import mu.KotlinLogging
 import no.nav.hjelpemidler.delbestilling.Database
+import no.nav.hjelpemidler.delbestilling.delbestilling.Hjelpemiddel
 import no.nav.hjelpemidler.delbestilling.delbestilling.toLagretDelbestilling
+import no.nav.hjelpemidler.delbestilling.hjelpemidler.HjelpemiddelDeler
 import javax.sql.DataSource
 
 private val logg = KotlinLogging.logger {}
 
 class RunOnStart(private val ds: DataSource = Database.migratedDataSource) {
     fun importNavn() {
+        val linjer = object {}.javaClass.getResourceAsStream("/hjelpemidler.txt")!!.bufferedReader().readLines()
+
+        val hjelpemidler = linjer.map { linje ->
+            val (hmsnr, navn, type) = linje.split("|")
+            Hjelpemiddel(hmsnr, navn, type)
+        }
+
         val delbestillinger = using(sessionOf(ds)) { session ->
             session.run(
                 queryOf(
@@ -25,6 +34,8 @@ class RunOnStart(private val ds: DataSource = Database.migratedDataSource) {
 
         delbestillinger.forEach {
             logg.info { it }
+            val navnHovedprodukt = HjelpemiddelDeler.hentHjelpemiddelMedDeler(it.delbestilling.hmsnr)?.navn ?: "Ukjent"
+            logg.info { "navnHovedprodukt: $navnHovedprodukt" }
         }
     }
 }
