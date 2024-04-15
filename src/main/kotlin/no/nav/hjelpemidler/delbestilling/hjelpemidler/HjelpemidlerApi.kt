3de91fba502da9f1763b68d7dc8b69eec5479f05
Ole Steinar Lillestøl Skrede <ole.steinar.lillestol.skrede@nav.no>
2024-04-15 14:51:57 +0200

    legg til info om når dellisten sist ble oppdatert

--- src/main/kotlin/no/nav/hjelpemidler/delbestilling/hjelpemidler/HjelpemidlerApi.kt
+++ src/main/kotlin/no/nav/hjelpemidler/delbestilling/hjelpemidler/HjelpemidlerApi.kt
@@ -4,6 +4,7 @@ import io.ktor.server.application.call
 import io.ktor.server.response.respond
 import io.ktor.server.routing.Route
 import io.ktor.server.routing.get
+import no.nav.hjelpemidler.delbestilling.hjelpemidler.data.DELER_SIST_OPPDATERT
 import no.nav.hjelpemidler.delbestilling.hjelpemidler.data.hjmNavn2Deler
 import no.nav.hjelpemidler.delbestilling.hjelpemidler.data.hjmType2Deler
 
@@ -18,4 +19,14 @@ fun Route.hjelpemiddelApi(
     get("/deler") {
         call.respond(hjmType2Deler)
     }
+
+    get("/deler-v2") {
+        call.respond(
+            mapOf(
+                "oppdatert" to DELER_SIST_OPPDATERT,
+                "deler" to hjmType2Deler,
+            )
+        )
+    }
+
 }
