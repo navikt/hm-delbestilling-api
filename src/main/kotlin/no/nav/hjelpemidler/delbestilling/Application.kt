d49d0978edd565f6b0dbef726e716eea65718d9f
Ole Steinar Lillestøl Skrede <ole.steinar.lillestol.skrede@nav.no>
2025-03-24 14:03:37 +0100

    skru av rapportering

--- src/main/kotlin/no/nav/hjelpemidler/delbestilling/Application.kt
+++ src/main/kotlin/no/nav/hjelpemidler/delbestilling/Application.kt
@@ -2,14 +2,11 @@ package no.nav.hjelpemidler.delbestilling
 
 import io.ktor.server.application.Application
 import io.ktor.server.application.ApplicationCall
-import io.ktor.server.application.ApplicationStarted
 import io.ktor.server.auth.authenticate
 import io.ktor.server.plugins.ratelimit.RateLimitName
 import io.ktor.server.plugins.ratelimit.rateLimit
 import io.ktor.server.routing.route
 import io.ktor.server.routing.routing
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.runBlocking
 import io.github.oshai.kotlinlogging.KotlinLogging
 import no.nav.hjelpemidler.delbestilling.delbestilling.azureRoutes
 import no.nav.hjelpemidler.delbestilling.delbestilling.delbestillingApiAuthenticated
@@ -17,7 +14,6 @@ import no.nav.hjelpemidler.delbestilling.delbestilling.delbestillingApiPublic
 import no.nav.hjelpemidler.delbestilling.hjelpemidler.data.validerData
 import no.nav.hjelpemidler.delbestilling.plugins.medDelbestillerRolle
 import no.nav.hjelpemidler.delbestilling.hjelpemidler.hjelpemiddelApi
-import no.nav.hjelpemidler.delbestilling.rapport.Rapportering
 import no.nav.tms.token.support.azure.validation.AzureAuthenticator
 import no.nav.tms.token.support.tokenx.validation.TokenXAuthenticator
 import no.nav.tms.token.support.tokenx.validation.user.TokenXUserFactory
@@ -57,7 +53,7 @@ fun Application.setupRoutes() {
         internal()
     }
 
-    Rapportering().rapporter()
+    // Rapportering().rapporter()
 }
 
 fun ApplicationCall.tokenXUser() = TokenXUserFactory.createTokenXUser(this)
