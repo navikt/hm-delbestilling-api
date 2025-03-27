6129db1e62233d9410a3124675dfcf2c0feac1fb
Håkon <hakonhagen2@gmail.com>
2025-03-27 14:26:31 +0100

    trenger ikke defaulte til emptyList()

--- src/main/kotlin/no/nav/hjelpemidler/delbestilling/delbestilling/DelbestillingModel.kt
+++ src/main/kotlin/no/nav/hjelpemidler/delbestilling/delbestilling/DelbestillingModel.kt
@@ -59,7 +59,7 @@ data class Del(
     val defaultAntall: Int = defaultAntall(kategori),
     val maksAntall: Int, // TODO kan ofte utlede maksAntall fra kategori også
     val img: String? = null, // Skal deprecates til fordel for imgs
-    val imgs: List<String> = emptyList(),
+    val imgs: List<String>,
     val datoLagtTil: LocalDate? = null,
     var lagerstatus: Lagerstatus? = null, // TODO: denne bør kanskje ikke være nullable?
     val kilde: Kilde? = Kilde.MANUELL_LISTE,
