2e7c9e7cbbfa50cb78507b3642e66d9679cd4420
Håkon <hakonhagen2@gmail.com>
2025-03-31 13:02:54 +0200

    debug log piloter

--- src/main/kotlin/no/nav/hjelpemidler/delbestilling/delbestilling/Piloter.kt
+++ src/main/kotlin/no/nav/hjelpemidler/delbestilling/delbestilling/Piloter.kt
@@ -1,6 +1,9 @@
 package no.nav.hjelpemidler.delbestilling.delbestilling
 
+import io.github.oshai.kotlinlogging.KotlinLogging
 import no.nav.hjelpemidler.delbestilling.isDev
+
+private val log = KotlinLogging.logger { }
 
 private const val kommunenummerOslo = "0301"
 
@@ -9,6 +12,8 @@ fun hentPiloter(brukersKommunenummer: String): List<Pilot> {
     if (brukersKommunenummer == kommunenummerOslo || isDev()) {
         piloter.add(Pilot.BESTILLE_IKKE_FASTE_LAGERVARER)
     }
+
+    log.info{"piloter: $piloter"}
 
     return piloter
 }
