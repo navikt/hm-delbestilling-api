2990c619024545c6120ed9783c78b5fd08f8b629
Håkon <hakonhagen2@gmail.com>
2025-03-31 14:51:13 +0200

    Revert "legg tilbake alle felt for Lagerstatus, for enklere debugging"
    This reverts commit 81cc3e17d7fb4b3f6e26985bc2d3e551c70b639d.

--- src/main/kotlin/no/nav/hjelpemidler/delbestilling/delbestilling/DelbestillingModel.kt
+++ src/main/kotlin/no/nav/hjelpemidler/delbestilling/delbestilling/DelbestillingModel.kt
@@ -76,27 +76,12 @@ enum class Kilde {
 }
 
 data class Lagerstatus(
-    val antallPåLager: Int,
-    val erPåLager: Boolean,
-
     val organisasjons_id: Int,
     val organisasjons_navn: String,
     val artikkelnummer: String,
-    val artikkelid: Int,
-    val fysisk: Int,
-    val tilgjengeligatt: Int,
-    val tilgjengeligroo: Int,
-    val tilgjengelig: Int,
-    val behovsmeldt: Int,
-    val reservert: Int,
-    val restordre: Int,
-    val bestillinger: Int,
-    val anmodning: Int,
-    val intanmodning: Int,
-    val forsyning: Int,
-    val sortiment: Boolean,
-    val lagervare: Boolean,
     val minmax: Boolean,
+    val tilgjengelig: Int,
+    val antallDelerPåLager: Int,
 )
 
 data class DelLinje(
