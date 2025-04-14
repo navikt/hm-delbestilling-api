1ae3b41699bc6cb32eff448f675bc2db0801758a
Håkon <hakonhagen2@gmail.com>
2025-04-14 08:31:16 +0200

    Fix formatering av Slack-melding

--- src/main/kotlin/no/nav/hjelpemidler/delbestilling/slack/SlackClient.kt
+++ src/main/kotlin/no/nav/hjelpemidler/delbestilling/slack/SlackClient.kt
@@ -60,7 +60,7 @@ class SlackClient(
             if (delerFraGrunndata.isNotEmpty()) {
                 var message =
                     "En delbestilling har kommet inn med deler fra grunndata, i ${brukersKommunenavn} kommune! Disse delene var: ```${
-                        delerFraGrunndata.joinToString("\n") { "`${it.del.hmsnr} ${it.del.navn}`" }
+                        delerFraGrunndata.joinToString("\n") { "${it.del.hmsnr} ${it.del.navn}" }
                     }```"
 
                 val delerIManuellListe = hmsnrTilDel.values.toList()
@@ -68,7 +68,7 @@ class SlackClient(
                     delerFraGrunndata.filter { del -> delerIManuellListe.find { it.hmsnr == del.del.hmsnr } != null }
 
                 message += if (delerSomOgsåFinnesIManuellListe.isNotEmpty()) {
-                    ".\nFølgende deler finnes også i manuell liste: ```${
+                    "Følgende deler finnes også i manuell liste: ```${
                         delerSomOgsåFinnesIManuellListe.joinToString(
                             "\n"
                         ) { "${it.del.hmsnr} ${it.del.navn}" }
