e5e08d586101dd9b111897dd5c0f1a6a11495f5c
Håkon <hakonhagen2@gmail.com>
2025-04-24 08:48:39 +0200

    Skrur av pilot ikke-fast lagervare i påvente av fiks for lagerstatus

--- src/main/kotlin/no/nav/hjelpemidler/delbestilling/delbestilling/PiloterService.kt
+++ src/main/kotlin/no/nav/hjelpemidler/delbestilling/delbestilling/PiloterService.kt
@@ -3,7 +3,7 @@ import no.nav.hjelpemidler.delbestilling.delbestilling.model.Pilot
 import no.nav.hjelpemidler.delbestilling.config.isDev
 import no.nav.hjelpemidler.hjelpemidlerdigitalSoknadapi.tjenester.norg.NorgService
 
-private val PILOTENHETER_BESTILLE_IKKE_FASTE_LAGERVARER = listOf(ENHETNR_OSLO, ENHETNR_ØST_VIKEN)
+private val PILOTENHETER_BESTILLE_IKKE_FASTE_LAGERVARER = emptyList<String>()
 
 class PiloterService(
     private val norgService: NorgService
