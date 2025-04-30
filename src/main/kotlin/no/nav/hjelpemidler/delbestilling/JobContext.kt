e32374433bce527c22f328f2bc3b6d0a0b2adc13
Ole Steinar Lillestøl Skrede <ole.steinar.lillestol.skrede@nav.no>
2025-04-30 09:54:24 +0200

    Øk timout for kjøring av anmodningsrapport

--- src/main/kotlin/no/nav/hjelpemidler/delbestilling/JobContext.kt
+++ src/main/kotlin/no/nav/hjelpemidler/delbestilling/JobContext.kt
@@ -1,6 +1,16 @@
 package no.nav.hjelpemidler.delbestilling
 
-import no.nav.hjelpemidler.delbestilling.infrastructure.defaultHttpClient
+import io.ktor.client.engine.cio.CIO
+import io.ktor.client.plugins.HttpRequestRetry
+import io.ktor.client.plugins.HttpTimeout
+import io.ktor.client.plugins.defaultRequest
+import io.ktor.client.plugins.logging.LogLevel
+import io.ktor.client.plugins.logging.Logging
+import io.ktor.client.request.accept
+import io.ktor.http.ContentType
+import io.ktor.http.contentType
+import no.nav.hjelpemidler.delbestilling.config.isProd
+import no.nav.hjelpemidler.http.createHttpClient
 import no.nav.hjelpemidler.http.openid.azureADClient
 import kotlin.time.Duration.Companion.seconds
 
@@ -12,6 +22,27 @@ class JobContext {
         }
     }
 
-    val client = defaultHttpClient()
+    val client = createHttpClient(engine = CIO.create()) {
+        expectSuccess = true
+
+        install(HttpRequestRetry) {
+            retryOnExceptionOrServerErrors(maxRetries = 5)
+            exponentialDelay()
+        }
+
+        install(HttpTimeout) {
+            requestTimeoutMillis = 60.seconds.inWholeMilliseconds
+        }
+
+
+        install(Logging) {
+            level = if (isProd()) LogLevel.INFO else LogLevel.BODY
+        }
+
+        defaultRequest {
+            accept(ContentType.Application.Json)
+            contentType(ContentType.Application.Json)
+        }
+    }
 
 }
