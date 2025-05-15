779fa6bf94c842a8e3f25e3b78882ed6d2f612a2
Ole Steinar Lillestøl Skrede <ole.steinar.lillestol.skrede@nav.no>
2025-05-15 16:25:51 +0200

    Legg til enhetsnr for alle enheter

--- src/main/kotlin/no/nav/hjelpemidler/delbestilling/common/Enhet.kt
+++ src/main/kotlin/no/nav/hjelpemidler/delbestilling/common/Enhet.kt
@@ -3,26 +3,27 @@ package no.nav.hjelpemidler.delbestilling.common
 import no.nav.hjelpemidler.delbestilling.config.isDev
 
 
-enum class Enhet(val nummer: String, private val epost: String) {
+enum class Enhet(val nummer: String, private val epost: String?) {
 
     ØST_VIKEN("4701", "nav.hot.ost-viken.teknisk.ordrekontor@nav.no"),
     OSLO("4703", "nav.hot.oslo.teknisk.ordrekontor@nav.no"),
-//    INNLANDET_ELVERUM("4704", ""),
-//    INNLANDET_GJØVIK("4705", ""),
-//    VEST_VIKEN("4706", ""),
-//    VESTFOLD_OG_TELEMARK("4707", ""),
-//    AGDER("4710", ""),
-//    ROGALAND("4711", ""),
-//    VESTLAND_BERGEN("4712", ""),
-//    VESTLAND_FØRDE("4714", ""),
-//    MØRE_OG_ROMSDAL("4715", ""),
-//    TRØNDELAG("4716", ""),
-//    NORDLAND("4718", ""),
-//    TROMS_OG_FINNMARK("4719", ""),
+    INNLANDET_ELVERUM("4704", null),
+    INNLANDET_GJØVIK("4705", null),
+    VEST_VIKEN("4706", null),
+    VESTFOLD_OG_TELEMARK("4707", null),
+    AGDER("4710", null),
+    ROGALAND("4711", null),
+    VESTLAND_BERGEN("4712", null),
+    VESTLAND_FØRDE("4714", null),
+    MØRE_OG_ROMSDAL("4715", null),
+    TRØNDELAG("4716", null),
+    NORDLAND("4718", null),
+    TROMS_OG_FINNMARK("4719", null),
     ;
 
     fun epost(): String =
         if (isDev()) "digitalisering.av.hjelpemidler.og.tilrettelegging@nav.no" else this.epost
+            ?: throw IllegalArgumentException("Enhet $this, mangler epost")
 
 
     companion object {
