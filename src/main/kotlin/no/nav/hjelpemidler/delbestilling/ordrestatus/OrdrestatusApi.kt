6e86b359393a869bc8fd4328fa5ecd817145a370
Ole Steinar Lillestøl Skrede <ole.steinar.lillestol.skrede@nav.no>
2025-05-27 14:35:11 +0200

    Ignorer feil under statusoppdatering i dev.
    OeBS sender oss av og til inkonsistente data i dev ifbm. reset av OeBS
    i testmiljø. Ignorerer disse.

--- src/main/kotlin/no/nav/hjelpemidler/delbestilling/ordrestatus/OrdrestatusApi.kt
+++ src/main/kotlin/no/nav/hjelpemidler/delbestilling/ordrestatus/OrdrestatusApi.kt
@@ -7,6 +7,7 @@ import io.ktor.server.response.respond
 import io.ktor.server.routing.Route
 import io.ktor.server.routing.put
 import io.ktor.server.util.getOrFail
+import no.nav.hjelpemidler.delbestilling.config.isDev
 
 private val log = KotlinLogging.logger {}
 
@@ -18,10 +19,19 @@ fun Route.ordrestatusRoutes(
         val (status, oebsOrdrenummer) = call.receive<StatusOppdateringRequest>()
         log.info { "Oppdaterer status for delbestilling $id (hmdel_$id) til status $status" }
 
+        try {
+            delbestillingStatusService.oppdaterStatus(id, status, oebsOrdrenummer)
+            call.respond(HttpStatusCode.OK)
+            log.info { "Status for delbestilling $id (hmdel_$id) oppdatert OK" }
+        } catch (e: Exception) {
+            if (isDev()) {
+                log.info(e) { "Ignorerer feil under statusoppdatering i dev. Antar ugyldige data fra OeBS." }
+                call.respond(HttpStatusCode.OK)
+            } else {
+                throw e
+            }
+        }
 
-        delbestillingStatusService.oppdaterStatus(id, status, oebsOrdrenummer)
-        call.respond(HttpStatusCode.OK)
-        log.info { "Status for delbestilling $id (hmdel_$id) oppdatert OK" }
     }
 
     put("/delbestilling/status/dellinje/{oebsOrdrenummer}") {
