12dfd527e947ff9a765104c0a914077f7beeb155
Håkon <hakonhagen2@gmail.com>
2025-09-09 12:09:52 +0200

    fix garantiberegning, log

--- src/main/kotlin/no/nav/hjelpemidler/delbestilling/oppslag/BerikMedGaranti.kt
+++ src/main/kotlin/no/nav/hjelpemidler/delbestilling/oppslag/BerikMedGaranti.kt
@@ -10,12 +10,15 @@ private val log = KotlinLogging.logger { }
 suspend fun berikMedGaranti(hjelpemiddel: Hjelpemiddel, utlån: Utlån): Hjelpemiddel {
     val garantiPeriodeStart = utlån.opprettetDato.tilOpprettetDato() // I OeBS er opprettet dato det samme som garantiperiode-start
 
-    val antallÅrGaranti = when(utlån.isokode.take(4)) {
+    val isokode = utlån.isokode.take(4)
+
+    val antallÅrGaranti = when(isokode) {
         "1223" -> 3 // 1223 = Motordrevne rullestoler (ERS) har garantitid på 3 år
         else -> 2
     }.toLong()
 
-    val garantiPeriodeSlutt = garantiPeriodeStart.plusYears(2)
-    // TODO: Støtte garantiperiode for ERS
+    log.info { "antallÅrGaranti for $isokode: $antallÅrGaranti" }
+
+    val garantiPeriodeSlutt = garantiPeriodeStart.plusYears(antallÅrGaranti)
     return hjelpemiddel.copy(erInnenforGaranti = LocalDate.now().isBefore(garantiPeriodeSlutt))
 }
