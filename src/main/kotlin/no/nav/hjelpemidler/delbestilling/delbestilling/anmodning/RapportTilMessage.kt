848609ba209e9c0f91e791d6fcbfde86d5c0aeb4
Håkon <hakonhagen2@gmail.com>
2025-09-22 15:19:16 +0200

    Fjern ekstra linjer i e-post hvis `trøndelagLagerInfo` er tomt

--- src/main/kotlin/no/nav/hjelpemidler/delbestilling/delbestilling/anmodning/RapportTilMessage.kt
+++ src/main/kotlin/no/nav/hjelpemidler/delbestilling/delbestilling/anmodning/RapportTilMessage.kt
@@ -15,10 +15,10 @@ ${anmodninger.joinToString("\n") { "${it.hmsnr} (${it.navn}): Må anmodes ${it.a
     // Fordi lager Sør-Trøndelag (4716) og Nord-Trøndelag (4717) deler e-post må vi legge til litt ekstra info her.
     val trøndelagLagerInfo = when (rapport.lager.nummer) {
         Lager.SØR_TRØNDELAG.nummer -> {
-"OBS: Disse delene skal leveres fra lager Sør-Trøndelag og må anmodes derfra."
+"\n\nOBS: Disse delene skal leveres fra lager Sør-Trøndelag og må anmodes derfra."
         }
         Lager.NORD_TRØNDELAG.nummer -> {
-"OBS: Disse delene skal leveres fra lager Nord-Trøndelag og må anmodes derfra."
+"\n\nOBS: Disse delene skal leveres fra lager Nord-Trøndelag og må anmodes derfra."
         }
         else -> ""
     }
@@ -28,9 +28,7 @@ Hei!
 
 Disse delene er bestilt digitalt, men er ikke på lager. Dere må derfor sende anmodning på følgende:
 
-${leverandørMeldinger.joinToString("\n\n") { it }}
-
-${trøndelagLagerInfo}
+${leverandørMeldinger.joinToString("\n\n") { it }}${trøndelagLagerInfo}
 
 Dersom dere har spørsmål til dette så kan dere svare oss tilbake på denne e-posten.
 
