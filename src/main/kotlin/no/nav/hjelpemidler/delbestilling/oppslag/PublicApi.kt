8f3d782da85d864c2372390f15115402968fb003
Ole Steinar Lillestøl Skrede <3756410+olesls@users.noreply.github.com>
2026-06-30 14:34:54 +0200

    prefix public endepunkt med 'public/'

--- src/main/kotlin/no/nav/hjelpemidler/delbestilling/oppslag/PublicApi.kt
+++ src/main/kotlin/no/nav/hjelpemidler/delbestilling/oppslag/PublicApi.kt
@@ -9,6 +9,7 @@ import io.ktor.server.response.respond
 import io.ktor.server.routing.Route
 import io.ktor.server.routing.get
 import io.ktor.server.routing.post
+import io.ktor.server.routing.route
 
 
 private val log = KotlinLogging.logger {}
@@ -17,37 +18,39 @@ fun Route.publicApi(
     hjelpemiddeloversikt: Hjelpemiddeloversikt,
     oppslagService: OppslagService,
 ) {
-    get("/tilgjengelige-hjelpemidler") {
-        call.respond(hjelpemiddeloversikt.hentTilgjengeligeHjelpemidlerCached())
-    }
+    route("/public") {
+        get("/tilgjengelige-hjelpemidler") {
+            call.respond(hjelpemiddeloversikt.hentTilgjengeligeHjelpemidlerCached())
+        }
 
-    post("/deler-til-hmsnrs") {
-        val hmsnrs = call.receive<DelerTilHmsnrsRequest>().hmsnrs
-        call.respond(hjelpemiddeloversikt.hentDelerTilHmsnrs(hmsnrs))
-    }
+        post("/deler-til-hmsnrs") {
+            val hmsnrs = call.receive<DelerTilHmsnrsRequest>().hmsnrs
+            call.respond(hjelpemiddeloversikt.hentDelerTilHmsnrs(hmsnrs))
+        }
 
-    rateLimit(RateLimitName("rateLimitOppslag")) {
-        post("/oppslag") {
-            val request = call.receive<OppslagRequest>()
-            log.info { "/oppslag request: $request" }
-            when (val result = oppslagService.slåOppHjelpemiddelMedSerienr(request.hmsnr, request.serienr)) {
-                is OppslagResult.Suksess -> call.respond(result.resultat)
-                is OppslagResult.Feil -> {
-                    log.info { "Oppslag feilet: ${result.feil}" }
-                    call.respond(HttpStatusCode.NotFound, OppslagFeilResponse(result.feil))
+        rateLimit(RateLimitName("rateLimitOppslag")) {
+            post("/oppslag") {
+                val request = call.receive<OppslagRequest>()
+                log.info { "/oppslag request: $request" }
+                when (val result = oppslagService.slåOppHjelpemiddelMedSerienr(request.hmsnr, request.serienr)) {
+                    is OppslagResult.Suksess -> call.respond(result.resultat)
+                    is OppslagResult.Feil -> {
+                        log.info { "Oppslag feilet: ${result.feil}" }
+                        call.respond(HttpStatusCode.NotFound, OppslagFeilResponse(result.feil))
+                    }
                 }
             }
         }
-    }
 
-    get("/hjelpemidler/{hmsnr}") {
-        val hmsnr = call.parameters["hmsnr"] ?: throw IllegalArgumentException("Mangler hmsnr")
-        log.info { "GET /hjelpemidler/$hmsnr" }
-        when (val result = oppslagService.slåOppHjelpemiddel(hmsnr)) {
-            is OppslagResultUtenDeler.Suksess -> call.respond(result.resultat)
-            is OppslagResultUtenDeler.Feil -> {
-                log.info { "Oppslag feilet: ${result.feil}" }
-                call.respond(HttpStatusCode.NotFound, OppslagFeilResponse(result.feil))
+        get("/hjelpemidler/{hmsnr}") {
+            val hmsnr = call.parameters["hmsnr"] ?: throw IllegalArgumentException("Mangler hmsnr")
+            log.info { "GET /hjelpemidler/$hmsnr" }
+            when (val result = oppslagService.slåOppHjelpemiddel(hmsnr)) {
+                is OppslagResultUtenDeler.Suksess -> call.respond(result.resultat)
+                is OppslagResultUtenDeler.Feil -> {
+                    log.info { "Oppslag feilet: ${result.feil}" }
+                    call.respond(HttpStatusCode.NotFound, OppslagFeilResponse(result.feil))
+                }
             }
         }
     }
