ea50a32a378d487f4b9cc631cbe4afdea1cd8a1b
Christian Bjørnsrud <christian.kristensen.bjornsrud@nav.no>
2026-09-11 09:43:27 +0200

    Fjern ubrukt import.

--- src/main/kotlin/no/nav/hjelpemidler/delbestilling/oppslag/PublicApi.kt
+++ src/main/kotlin/no/nav/hjelpemidler/delbestilling/oppslag/PublicApi.kt
@@ -9,7 +9,6 @@ import io.ktor.server.response.respond
 import io.ktor.server.routing.Route
 import io.ktor.server.routing.get
 import io.ktor.server.routing.post
-import io.ktor.server.routing.route
 
 
 private val log = KotlinLogging.logger {}
