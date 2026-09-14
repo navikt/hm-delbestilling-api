dbe502b1516c73ca37786a94f415929790332e74
Christian Bjørnsrud <christian.kristensen.bjornsrud@nav.no>
2026-09-14 15:22:43 +0200

    Sett markering av sendt melding i egen try/catch, for å unngå at catchblokk markerer at epost ikke er sendt om epost sendes korrekt, og databaseupdate feiler.

--- src/main/kotlin/no/nav/hjelpemidler/delbestilling/infrastructure/epostoutbox/EpostOutboxDispatcher.kt
+++ src/main/kotlin/no/nav/hjelpemidler/delbestilling/infrastructure/epostoutbox/EpostOutboxDispatcher.kt
@@ -1,8 +1,6 @@
 package no.nav.hjelpemidler.delbestilling.infrastructure.epostoutbox
 
 import io.github.oshai.kotlinlogging.KotlinLogging
-import no.nav.hjelpemidler.delbestilling.config.AppConfig
-import no.nav.hjelpemidler.delbestilling.config.isDev
 import no.nav.hjelpemidler.delbestilling.infrastructure.email.ContentType
 import no.nav.hjelpemidler.delbestilling.infrastructure.email.Email
 import no.nav.hjelpemidler.delbestilling.infrastructure.persistence.transaction.Transactional
@@ -25,15 +23,28 @@ class EpostOutboxDispatcher(
         val meldinger = transaction { epostOutboxDao.hentPending(BATCH_SIZE) }
 
         for (melding in meldinger) {
-            try {
+            val sendt = try {
                 email.send(melding.mottaker, melding.emne, melding.html, ContentType.HTML)
-                transaction { epostOutboxDao.markerSendt(melding.id) }
+                log.info { "E-post for manuell delbestilling med id ${melding.id} sendt til ${melding.mottaker}" }
+                true
             } catch (e: Exception) {
                 val nyeAttempts = melding.attempts + 1
                 val skalVarsle = nyeAttempts >= SLACK_VARSEL_TERSKEL && !melding.alerted
                 log.error(e) { "E-postutsending av outbox-melding ${melding.id} feilet (forsøk $nyeAttempts)" }
                 transaction { epostOutboxDao.registrerFeil(melding.id, e.message ?: e.javaClass.name, skalVarsle) }
                 if (skalVarsle) slack.varsleOmOutboxFeil("epost-${melding.id}", "Epost", nyeAttempts)
+                false
+            }
+
+            if (sendt) {
+                try {
+                    transaction { epostOutboxDao.markerSendt(melding.id) }
+                    log.info { "E-post med id ${melding.id} markert som sendt til ${melding.mottaker}" }
+                } catch (e: Exception) {
+                    log.error(e) {
+                        "Klarte ikke markere e-post ${melding.id} som sendt etter vellykket e-postutsending. E-post vil sendes på nytt."
+                    }
+                }
             }
         }
     }
